2fd20821fd770022ff41aa1d72e29df45aacf07e
Naveen Raghuvanshi <naveen@olecons.com>
2020-08-03 10:49:00 +0530

    update

--- config.js
+++ config.js
@@ -1,1 +1,14 @@
 server_url = "http://192.168.43.120:8000"
+storage_url = "http://192.168.43.120:3000"
+
+String.prototype.toDuration = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours+':'+minutes+':'+seconds;
+}
